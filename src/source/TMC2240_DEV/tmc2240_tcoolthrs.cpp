653388a11c79b5513a2116e1810d8877c50bb6f1
Scott Lahteine <thinkyhead@users.noreply.github.com>
2025-05-21 16:37:17 -0500

    ✨ TMC2240 SPI ✨
    Co-Authored-By: David Buezas <777196+dbuezas@users.noreply.github.com>
    Co-Authored-By: z1996xm <102506464+z1996xm@users.noreply.github.com>

---
+++ src/source/TMC2240_DEV/tmc2240_tcoolthrs.cpp
@@ -0,0 +1,21 @@
+/**
+ * TMCStepper library by @teemuatlut
+ */
+#ifdef TMC2240_DEV
+
+#include "../TMC_MACROS.h"
+#include "../../TMCStepper.h"
+
+#define GET_REG(SETTING) TMC2240_n::TCOOLTHRS_t r{0}; r.sr = TCOOLTHRS(); return r.SETTING
+#define SET_REG(SETTING) TCOOLTHRS_register.SETTING = B; write(TCOOLTHRS_register.address, TCOOLTHRS_register.sr)
+
+uint32_t TMC2240Stepper::TCOOLTHRS() { return TCOOLTHRS_register.sr; }
+void TMC2240Stepper::TCOOLTHRS(uint32_t data) {
+	TCOOLTHRS_register.sr = data;
+	write(TCOOLTHRS_register.address, TCOOLTHRS_register.sr);
+}
+
+void TMC2240Stepper::tcoolthrs(uint8_t B)	{ SET_REG(tcoolthrs); 	}
+uint8_t TMC2240Stepper::tcoolthrs()			{ GET_REG(tcoolthrs); 	}
+
+#endif // TMC2240_DEV
---
+++ src/source/TMC2240_DEV/tmc2240_tpwmthrs.cpp
@@ -0,0 +1,21 @@
+/**
+ * TMCStepper library by @teemuatlut
+ */
+#ifdef TMC2240_DEV
+
+#include "../TMC_MACROS.h"
+#include "../../TMCStepper.h"
+
+#define GET_REG(SETTING) TMC2240_n::TPWMTHRS_t r{0}; r.sr = TPWMTHRS(); return r.SETTING
+#define SET_REG(SETTING) TPWMTHRS_register.SETTING = B; write(TPWMTHRS_register.address, TPWMTHRS_register.sr)
+
+uint32_t TMC2240Stepper::TPWMTHRS() { return TPWMTHRS_register.sr; }
+void TMC2240Stepper::TPWMTHRS(uint32_t data) {
+	TPWMTHRS_register.sr = data;
+	write(TPWMTHRS_register.address, TPWMTHRS_register.sr);
+}
+
+void TMC2240Stepper::tpwmthrsb(uint8_t B) { SET_REG(tpwmthrsb); }
+uint8_t TMC2240Stepper::tpwmthrsb() { GET_REG(tpwmthrsb); }
+
+#endif // TMC2240_DEV
